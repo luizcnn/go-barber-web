38b6c6ad9503c64c5e41a2be6d068335a4e262ea
Luiz Bonfim <luiz.bonfim.neto@ccc.ufcg.edu.br>
2021-02-10 12:50:25 -0300

    Signin page and componentization

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+
+import logoImg from '../../assets/img/logo.svg';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Background, Container, Content } from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+        <form>
+          <h1>Faça seu Logon</h1>
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            type="password"
+            icon={FiLock}
+            placeholder="Senha"
+          />
+          <Button type="submit">Entrar</Button>
+          <a href="/forgot">Esqueci minha senha</a>
+        </form>
+        <a href="/">
+          <FiLogIn />
+          Criar conta
+        </a>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
+
+export default SignIn;
